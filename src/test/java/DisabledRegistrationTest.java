f12a612e632ffbf85cbe2abed2f532791de2cbc0
andreiPsarev <60147126+andreiPsarev@users.noreply.github.com>
2023-06-04 23:10:22 +0300

    Update DisabledRegistrationTest.java

--- src/test/java/DisabledRegistrationTest.java
+++ src/test/java/DisabledRegistrationTest.java
@@ -24,8 +24,8 @@ public class DisabledRegistrationTest {
 
   @Before
   public void setUp() {
-    String driverPath = "./chromedriver/chromedriver.exe";
-    System.setProperty("webdriver.chrome.driver", driverPath);
+  String driverPath = "src/test/resources/chromedriver/chromedriver.exe";
+  System.setProperty("webdriver.chrome.driver", driverPath);
     ChromeOptions options = new ChromeOptions();
     options.addArguments("--headless");
     driver = new ChromeDriver(options);
